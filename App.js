e094572e94ba9209dc73c419d380a933402c78f3
Susumu Ishikawa <susumu2934@gmail.com>
2020-02-29 16:19:23 +0900

    add style to bottom tabs

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Text, View, Button } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { Ionicons } from "@expo/vector-icons";
 
 import ProfileScreen from "./src/screens/ProfileScreen";
 
@@ -17,7 +18,24 @@ const Tab = createBottomTabNavigator();
 
 function Tabs() {
   return (
-    <Tab.Navigator>
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ color, size }) => {
+          let iconName;
+
+          if (route.name === "Home") {
+            iconName = "md-home";
+          } else if (route.name === "Profile") {
+            iconName = "md-person";
+          }
+          return <Ionicons name={iconName} size={size} color={color} />;
+        }
+      })}
+      tabBarOptions={{
+        activeTintColor: "tomato",
+        inactiveTintColor: "grey"
+      }}
+    >
       <Tab.Screen name="Home" component={HomeScreen} />
       <Tab.Screen name="Profile" component={ProfileScreen} />
     </Tab.Navigator>
