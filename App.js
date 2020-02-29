0f509ac0a3b862b1059ba1761574bba6da9bce62
Susumu Ishikawa <susumu2934@gmail.com>
2020-02-29 15:54:15 +0900

    add ProfileScreen

--- App.js
+++ App.js
@@ -1,19 +1,28 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { Text, View, Button } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+
+import ProfileScreen from "./src/screens/ProfileScreen";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Profile" component={ProfileScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+function HomeScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Text>Home Screen</Text>
+      <Button title="Profile" onPress={() => navigation.navigate("Profile")} />
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center"
-  }
-});
+const Stack = createStackNavigator();
