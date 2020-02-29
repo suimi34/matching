c6ff8613afd294419d0347aeae4c88e3bdbd929e
Susumu Ishikawa <susumu2934@gmail.com>
2020-02-29 16:05:04 +0900

    use tab navigator

--- App.js
+++ App.js
@@ -1,18 +1,26 @@
 import React from "react";
 import { Text, View, Button } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
-import { createStackNavigator } from "@react-navigation/stack";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 
 import ProfileScreen from "./src/screens/ProfileScreen";
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="Profile" component={ProfileScreen} />
-      </Stack.Navigator>
+      <Tabs />
     </NavigationContainer>
+  );
+}
+
+const Tab = createBottomTabNavigator();
+
+function Tabs() {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen name="Home" component={HomeScreen} />
+      <Tab.Screen name="Profile" component={ProfileScreen} />
+    </Tab.Navigator>
   );
 }
 
@@ -24,5 +32,3 @@ function HomeScreen({ navigation }) {
     </View>
   );
 }
-
-const Stack = createStackNavigator();
